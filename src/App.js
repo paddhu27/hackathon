c622459068018d1d13bccbfc17ab12534f9e13bd
paddhu27 <paddhuk27@gmail.com>
2023-03-11 17:20:41 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import MyForm from './components/profile';
+
+import Myprofile from './components/profile';
 import RegistrationForm from './components/signup';
 
 
 function App() {
   return (
     <div className="App">
-     
-      <MyForm/>
+     <Myprofile />
+      
     </div>
   );
 }
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,124 +1,78 @@
-/*import React,{ useState } from "react";
-import ReactDOM from "react-dom/client";
-
-function MyForm() {
-  const [inputs, setInputs] = useState({});
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}))
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(inputs);
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-        <div>
-    <label>Are you a fresher<input type="radio" name="fresher" value="true" onChange={handleChange} />Yes</label>
-    <div><input type="radio" name="fresher" value="true" onChange={handleChange} />no</div></div>
-
-      <label>Enter your name:
-      <input 
-        type="text" 
-        name="username" 
-        value={inputs.username || ""} 
-        onChange={handleChange}
-      />
-      </label>
-      <label>Enter your age:
-        <input 
-          type="number" 
-          name="age" 
-          value={inputs.age || ""} 
-          onChange={handleChange}
-        />
-        </label>
-        <input type="submit" />
-    </form>
-  )
-}
-
-export default MyForm;
-*/
-
-
 import  React,{ useState } from 'react';
 import './style.css'
 
-function Myform() {
-
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    occupation: '',
-    gender: '',
-    languages: ['html'],
-  })
-
-  const onChangeHandler = (event) => {
-
-    console.log(event)
-    if (event.target.name === 'languages') {
-
-      let copy = { ...formData }
-
-      if (event.target.checked) {
-        copy.languages.push(event.target.value)
-      } else {
-        copy.languages = copy.languages.filter(el => el !== event.target.value)
-      }
-
-      setFormData(copy)
-
-    } else {
-      setFormData(() => ({
-        ...formData,
-        [event.target.name]: event.target.value
-      }))
-    }
-  }
-
-  const onSubmitHandler = (event) => {
-    event.preventDefault()
-    console.log(formData)
-  }
+function Myprofile() {
+        const [education, setEducation] = useState(null);
+        const [branch, setBranch] = useState(null);
+        const [linkedin, setLinkedin] = useState(null);
+        const [gitlink,setGitlink] = useState(null);
+        const [skills,setSkills] = useState(null);
+        const [occupation,setOccupation] = useState(null);
+        const [experience,setExperience] = useState(null);
+    
+        const onChangeHandler = (e) => {
+            const {id , value} = e.target;
+            if(id === "education"){
+                setEducation(value);
+            }
+            if(id === "branch"){
+                setBranch(value);
+            }
+            if(id === "linkedin"){
+                setLinkedin(value);
+            }
+            if(id === "gitlink"){
+                setGitlink(value);
+            }
+            if(id === "skills"){
+                setSkills(value);
+            }
+            if(id === "occupation"){
+                setOccupation(value);
+            }
+            if(id === "experience"){
+                setExperience(value);
+            }
+    
+        }
+  
   return (
     <div className="profile">
       <nav class="head">
         <h1><b>Education & Skills Details</b></h1>
       </nav>
-      <form onSubmit={onSubmitHandler}>
+      <form >
         <div class='one1'>
         <div class='onee'>
         <div className="form-group">
-          <label htmlFor="education" className="form-label">Current Education</label>
-          <input className="form-control" name="education" onChange={onChangeHandler} value={formData.education} />
+          <label for="education" className="form-label">Current Education</label>
+          <input className="form-control" name="education" value={education} onChange={ (e)=>onChangeHandler(e)} id="education" placeholder="Education"/>
         </div>
         </div>
         
         <div className="form-group">
           <label htmlFor="branch" className="form-label">Branch</label>
-          <input className="form-control" name="branch" onChange={onChangeHandler} value={formData.branch} />
+          <input className="form-control" name="branch" value={branch} onChange={ (e)=>onChangeHandler(e)} id="branch" placeholder="Branch" />
         </div>
         </div>
   
         <div className="form-group">
-          <label htmlFor="username" className="form-label">Linkedin Profile</label>
-          <input className="form-control" name="username" onChange={onChangeHandler} value={formData.username} />
+          <label htmlFor="linkedin" className="form-label">Linkedin Profile</label>
+          <input className="form-control" name="linkedin" value={linkedin} onChange={ (e)=>onChangeHandler(e)} id="linkedin" placeholder="Linkedin Profile" />
         </div>
         <div className="form-group">
-          <label htmlFor="email" className="form-label">Github Profile</label>
-          <input className="form-control" name="email" onChange={onChangeHandler} value={formData.email} />
+          <label htmlFor="gitlink" className="form-label">Github Profile</label>
+          <input className="form-control" name="gitlink"  value={gitlink} onChange={ (e)=>onChangeHandler(e)} id="gitlink" placeholder="Github Link" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="skills" className="form-label">Please add your skills</label>
+          <input className="form-control" name="skills" value={skills} onChange={ (e)=>onChangeHandler(e)} id="skills" placeholder="Please Enter Your Skills"/>
         </div>
         <div class='box'>
         <div class="one">
         <div className="form-group">
           <label htmlFor="occupation" className="form-label">Occupation</label>
-          <select className="form-select" name="occupation" onChange={onChangeHandler} value={formData.occupation}>
+          <select className="form-select" name="occupation"  value={occupation} onChange={ (e)=>onChangeHandler(e)} id="occupation" placeholder="Occupation" >
             <option value="student">Student</option>
             <option value="employee">Employee</option>
             <option value="other">Recruiter</option>
@@ -127,7 +81,7 @@ function Myform() {
         </div>
         <div className="form-group">
           <label htmlFor="experience" className="form-label">Coding Experience</label>
-          <select className="form-select" name="experience" onChange={onChangeHandler} value={formData.experience}>
+          <select className="form-select" name="experience"  value={experience} onChange={ (e)=>onChangeHandler(e)} id="experience" placeholder="Experience">
             <option value="Beginner">Basic</option>
             <option value="Moderate">Intermediate</option>
             <option value="Talented">Talented</option>
@@ -140,6 +94,6 @@ function Myform() {
       </form>
     </div>
   );
-}
+  }
 
-export default Myform;
+export default Myprofile;
